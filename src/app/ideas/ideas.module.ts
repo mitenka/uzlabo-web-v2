1c2d19f403ba459b96f3f78bbced32966b859d28
Anna.Art <anna.art@cloudberrylab.com>
2020-09-07 15:26:38 +0300

    Page layout, ideas list, ng-select, etc

--- src/app/ideas/ideas.module.ts
+++ src/app/ideas/ideas.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 import { IdeasRoutingModule } from './ideas-routing.module';
 import { IdeaCardComponent } from './idea-card/idea-card.component';
 import { IdeaCardListComponent } from './idea-card-list/idea-card-list.component';
 import { IdeaWizardComponent } from './idea-wizard/idea-wizard.component';
 
-
 @NgModule({
   declarations: [IdeaCardComponent, IdeaCardListComponent, IdeaWizardComponent],
   imports: [
     CommonModule,
+    NgSelectModule,
+    FormsModule,
     IdeasRoutingModule
-  ]
+  ],
+  bootstrap: [IdeaCardListComponent]
 })
 export class IdeasModule { }
